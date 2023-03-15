0b034785a5c013478c9e408096486f62618db567
Alex Gehrin <agehrin@gmail.com>
2023-03-15 13:39:59 -0500

    Fixed load task command

--- Handlers/LoadTaskCommandHandler.cs
+++ Handlers/LoadTaskCommandHandler.cs
@@ -1,45 +1,48 @@
-﻿using BoxwriterResmarkInterop.Exceptions;
-using BoxwriterResmarkInterop.Extensions;
-using BoxwriterResmarkInterop.Interfaces;
-using BoxwriterResmarkInterop.OPCUA;
-using BoxwriterResmarkInterop.Requests;
-using MediatR;
-using Workstation.ServiceModel.Ua;
-
-namespace BoxwriterResmarkInterop.Handlers;
-
-public class LoadTaskCommandHandler : IRequestHandler<LoadTaskRequest, StringResponse>
+﻿namespace BoxwriterResmarkInterop.Handlers
 {
-    private readonly ILogger<LoadTaskCommandHandler> _logger;
-    private readonly IOPCUAService _opcuaService;
-    private const string CommandName = "Load task";
+    using Exceptions;
 
-    public LoadTaskCommandHandler(ILogger<LoadTaskCommandHandler> logger, IOPCUAService opcuaService)
+    using Extensions;
+
+    using Interfaces;
+
+    using MediatR;
+
+    using OPCUA;
+
+    using Requests;
+
+    using Workstation.ServiceModel.Ua;
+
+    public class LoadTaskCommandHandler : IRequestHandler<LoadTaskRequest, StringResponse>
     {
-        _logger = logger;
-        _opcuaService = opcuaService;
-    }
+        private const string CommandName = "Load task";
+        private readonly ILogger<LoadTaskCommandHandler> _logger;
+        private readonly IOPCUAService _opcuaService;
 
-    public async Task<StringResponse> Handle(LoadTaskRequest request, CancellationToken cancellationToken)
-    {
-        var printerId = request.Data.ExtractPrinterId();
-        var messageName = request.Data.ExtractMessageName();
-
-        var response =
-            await _opcuaService.CallMethodAsync(printerId, OPCUAMethods.ResumePrinting.ToString(), cancellationToken, messageName);
-
-        if (response is null)
+        public LoadTaskCommandHandler(ILogger<LoadTaskCommandHandler> logger, IOPCUAService opcuaService)
         {
-            _logger.LogError("Start task OPCUA call failed");
-
-            throw new OPCUACommunicationFailedException("Start task OPCUA call failed");
+            _logger = logger;
+            _opcuaService = opcuaService;
         }
 
-        return new StringResponse(CommandName, printerId, GetResponseData(response));
-    }
+        public async Task<StringResponse> Handle(LoadTaskRequest request, CancellationToken cancellationToken)
+        {
+            var printerId = request.Data.ExtractPrinterId();
+            var messageName = request.Data.ExtractMessageName();
 
-    private static bool GetResponseData(CallMethodResult result)
-    {
-        return StatusCode.IsGood(result.StatusCode);
+            var response = await _opcuaService.CallMethodAsync(printerId, OPCUAMethods.PrintStoredMessage.ToString(), cancellationToken, messageName);
+
+            if (response is null)
+            {
+                _logger.LogError("Start task OPCUA call failed");
+
+                throw new OPCUACommunicationFailedException("Start task OPCUA call failed");
+            }
+
+            return new StringResponse(CommandName, printerId, GetResponseData(response));
+        }
+
+        private static bool GetResponseData(CallMethodResult result) => StatusCode.IsGood(result.StatusCode);
     }
 }
