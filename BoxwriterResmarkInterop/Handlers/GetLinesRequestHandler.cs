af2fa4f31e0934ab1cd21a98e2541d803c111760
Alex Gehrin <agehrin@gmail.com>
2023-03-23 14:47:01 -0500

    converted to expression body in get lines request handler

--- BoxwriterResmarkInterop/Handlers/GetLinesRequestHandler.cs
+++ BoxwriterResmarkInterop/Handlers/GetLinesRequestHandler.cs
@@ -14,18 +14,10 @@ public class GetLinesRequestHandler : IRequestHandler<GetLinesRequest, StringRes
 {
     private readonly PrinterConnections _printerConnections;
 
-    public GetLinesRequestHandler(IOptions<PrinterConnections> configuration)
-    {
-        _printerConnections = configuration.Value;
-    }
+    public GetLinesRequestHandler(IOptions<PrinterConnections> configuration) => _printerConnections = configuration.Value;
 
-    public Task<StringResponse> Handle(GetLinesRequest request, CancellationToken cancellationToken)
-    {
-        return Task.FromResult(new StringResponse(GetLines, GetPrinters()));
-    }
+    public Task<StringResponse> Handle(GetLinesRequest request, CancellationToken cancellationToken) =>
+        Task.FromResult(new StringResponse(GetLines, GetPrinters()));
 
-    private IEnumerable<string> GetPrinters()
-    {
-        return _printerConnections.Printers.Select(printer => printer.PrinterId);
-    }
+    private IEnumerable<string> GetPrinters() => _printerConnections.Printers.Select(printer => printer.PrinterId);
 }
