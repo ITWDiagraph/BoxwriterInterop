a76febef6b0976925d54835b907a00b198dac417
Rich Moll <rmoll@diagraph.com>
2023-03-27 13:40:01 -0500

    Added warning log when the user has leading or trailing whitespace

--- BoxwriterResmarkInterop/Handlers/LoadTaskCommandHandler.cs
+++ BoxwriterResmarkInterop/Handlers/LoadTaskCommandHandler.cs
@@ -17,16 +17,23 @@ using static Constants;
 public class LoadTaskCommandHandler : IRequestHandler<LoadTaskRequest, StringResponse>
 {
     private readonly IOPCUAService _opcuaService;
+    private readonly ILogger<LoadTaskCommandHandler> _logger;
 
-    public LoadTaskCommandHandler(IOPCUAService opcuaService)
+    public LoadTaskCommandHandler(IOPCUAService opcuaService, ILogger<LoadTaskCommandHandler> logger)
     {
         _opcuaService = opcuaService;
+        _logger = logger;
     }
 
     public async Task<StringResponse> Handle(LoadTaskRequest request, CancellationToken cancellationToken)
     {
         var printerId = request.Data.ExtractPrinterId();
         var messageName = request.Data.ExtractAdditionalParameter();
+
+        if (messageName.StartsWith(" ") || messageName.EndsWith(" "))
+        {
+            _logger.LogWarning("Message name \"{MessageName}\" has leading or trailing spaces", messageName);
+        }
 
         var opcuaRequest = new OPCUARequest
         {
